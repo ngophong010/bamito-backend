fbb293fa4cc89f814d986081734ab77bcb7e2cb4
Phong Ngo <ngophong010@outlook.com>
2025-08-23 23:05:56 +0700

    refactor: refactor role model

---
+++ src/models/role.ts
@@ -0,0 +1,41 @@
+import { Sequelize, DataTypes, Model } from "sequelize";
+import type {Optional, HasManyGetAssociationsMixin} from "sequelize";
+import type { User } from "./user.js";
+import type { Models } from "./index.js";
+
+interface RoleAttributes {
+id: number;
+roleId: string;
+roleName: string;
+}
+
+type RoleCreationAttributes = Optional<RoleAttributes, "id">;
+export class Role extends Model<RoleAttributes, RoleCreationAttributes> implements RoleAttributes {
+  public id!: number;
+  public roleId!: string;
+  public roleName!: string;
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+  // Association methods
+  public getUsers!: HasManyGetAssociationsMixin<User>;
+  public static associate(models: Models) {
+    // A Role can be assigned to many Users
+    Role.hasMany(models.User, {
+      foreignKey: "roleId", // This will be an integer FK in the User model
+      sourceKey: "id", // It links to the integer PK of this Role model
+      as: "users",
+    });
+  }
+}
+export default function (sequelize: Sequelize): typeof Role {
+  Role.init({
+    id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+    roleId: { type: DataTypes.STRING, allowNull: false, unique: true },
+    roleName: { type: DataTypes.STRING, allowNull: false },
+  }, {
+    sequelize,
+    modelName: "Role",
+    tableName: "roles",
+  });
+  return Role;
+}
