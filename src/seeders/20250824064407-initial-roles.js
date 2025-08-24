9c5ac8151d80e1f8fd8df4dc9d665dd05e7091da
Phong Ngo Lam <ngophong010@gmail.com>
2025-08-24 15:28:04 +0700

    feat: add init roles seeder

---
+++ src/seeders/20250824064407-initial-roles.js
@@ -0,0 +1,44 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await queryInterface.bulkInsert(
+      "roles",
+      [
+        {
+          roleId: "R1",
+          roleName: "Admin",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          roleId: "R2",
+          roleName: "Customer",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('roles', null, {});
+  },
+};
