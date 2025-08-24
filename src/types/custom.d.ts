8cf20125f53dbdb0b23b4dc532bc50b3f44f123c
Phong Ngo Lam <ngophong010@gmail.com>
2025-08-24 17:30:16 +0700

    refactor: refactor custom type

--- src/types/custom.d.ts
+++ src/types/custom.d.ts
@@ -1,10 +1,11 @@
-// This tells TypeScript that we are augmenting the existing 'Express' namespace.
+interface JwtPayload {
+  id: number;
+  role: string;
+}
+
 declare namespace Express {
-  // We are adding a new property 'file' to the 'Request' interface.
   export interface Request {
-    // 'file' is optional because not every request will have a file upload.
-    // The type `Multer.File` comes from the Multer library itself,
-    // providing full type safety for file properties like .path, .filename, etc.
     file?: Multer.File;
+    user?: JwtPayload; // Add the user property
   }
 }
