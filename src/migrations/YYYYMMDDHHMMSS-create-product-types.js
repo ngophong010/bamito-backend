4bfbde03d02c2b3da454320f3e76df13747925e4
Phong Ngo Lam <ngophong010@gmail.com>
2025-08-28 21:07:34 +0700

    chor: remove redundant comment of product types migration

--- src/migrations/YYYYMMDDHHMMSS-create-product-types.js
+++ src/migrations/YYYYMMDDHHMMSS-create-product-types.js
@@ -1,22 +1,20 @@
 "use strict";
 
 /** @type {import('sequelize-cli').Migration} */
+
 module.exports = {
   async up(queryInterface, Sequelize) {
-    // ENHANCEMENT 2: Use the consistent, lowercase table name.
     await queryInterface.createTable("product_types", {
-      // ENHANCEMENT 1: 'id' is the single, integer primary key for performance.
       id: {
         type: Sequelize.INTEGER,
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
       },
-      // 'productTypeId' is now a regular column, but it must be unique.
       productTypeId: {
         type: Sequelize.STRING,
         allowNull: false,
-        unique: true, // This enforces the business rule.
+        unique: true,
       },
       productTypeName: {
         type: Sequelize.STRING,
