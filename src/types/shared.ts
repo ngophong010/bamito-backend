0f519938c8ec179ef1dca3fceee1b426c560faa1
Phong Ngo Lam <ngophong010@gmail.com>
2025-09-06 23:47:00 +0700

    feat: add ProductWithRating, ProductWithRatingAttributes, FavouriteWithProduct, UnreviewedItem interface

--- src/types/shared.ts
+++ src/types/shared.ts
@@ -1,4 +1,30 @@
+import { Product } from "../models/product.js";
+import type {  ProductAttributes } from "../models/product.js";
+import { Favourite } from "../models/favourite.js";
+
 export interface FilterOptions {
   brandId?: number[];
   price?: [number, number];
+}
+
+export interface ProductWithRating extends Product {
+  dataValues: ProductAttributes & { averageRating: string | null };
+}
+
+export interface ProductWithRatingAttributes extends ProductAttributes {
+  averageRating: string | null;
+}
+
+
+export interface FavouriteWithProduct extends Favourite {
+  ProductFavouriteData: ProductWithRating;
+}
+
+export interface UnreviewedItem {
+    productId: number;
+    orderId: number;
+    quantity: number;
+    totalPrice: number;
+    SizeOrderDetailData: { sizeId: number; sizeName: string; };
+    ProductDetailData: { image: string; name: string; price: number; discount: number; };
 }
